8a9699040e5f936b367c2fcf23c16e8131c766be
attila <attila@stalphonsos.com>
2015-07-20 22:37:51 -0500

    Skeletal script

--- www/tbb/tor-browser/files/start-tor-browser.sh
+++ www/tbb/tor-browser/files/start-tor-browser.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # -*- mode:sh; indent-tabs-mode:nil; sh-indentation:4 -*-
 ##
 # start-tor-browser.sh - start the tor browser
@@ -20,3 +21,74 @@
 # PERFORMANCE OF THIS SOFTWARE.
 ##
 
+## Init
+
+detach=0
+verbose=0
+logfile=""
+
+## Helper Routines
+
+# v=`optval option $string`
+# if $string looks like "--option=foo" then v will be "foo"
+#
+optval () {
+    _opt="$1"
+    shift
+    echo "$*" | sed -e "s/^--${_opt}=//"
+}
+
+setopt () {
+    _nm="$1"
+    _v=1
+    [ -n "$2" ] && _v=`optval ${_nm} "$2"`
+    eval $_nm=$_v
+}
+
+usage () {
+    _xit=0
+    [ -n "$1" ] && {
+        echo "$0: $*"
+        _xit=1
+    }
+    echo "usage: $0 [--options] [args]"
+    echo "       --help         this message"
+    exit $_xit
+}
+
+setup_dot_tor_browser () {
+}
+
+run_tor_browser () {
+}
+
+## Main
+
+while [ x"$1" != x ]; do
+    case "$1" in
+        --help)
+            usage
+            ;;
+        --detach)
+            setopt detach
+	    ;;
+        -v | --verbose | -d | --debug)
+            setopt verbose
+            ;;
+        --log)
+            ;;
+        --log=*)
+            ;;
+        --register-app)
+            ;;
+        --unregister-app)
+            ;;
+        *)
+            usage unrecongized option: "$1"
+            ;;
+    esac
+    shift
+done
+
+setup_dot_tor_browser
+run_tor_browser
